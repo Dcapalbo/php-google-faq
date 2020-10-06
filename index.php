79f44898b2c0d7dcd69e8df109cea005c37c7af3
Domenico Capalbo <capalbodomenico@gmail.com>
2020-10-06 14:31:24 +0200

    init
    php-google-faq

---
+++ index.php
@@ -0,0 +1,18 @@
+<!-- Riscrivere questa pagina del sito google https://policies.google.com/faq.
+Ci sono diverse domande con relative risposte. Gestire il “Database” e la visualizzazione di queste domande e risposte con PHP. -->
+
+<?php
+?>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="css/style.css">
+    <title></title>
+  </head>
+  <body>
+  </body>
+</html>
